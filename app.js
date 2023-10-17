dacb46bbfa7a5c61a85b1f80e79e2dc43488a4a2
yuki1582 <yuki.nakamura1103@gmail.com>
2023-10-18 00:06:01 +0900

    tsc union

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-// const person : {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// }= {
-//     name: "yota",
-//     age: 25,
-//     hobbies: ["soccer", "cooking"],
-//     role: [2, "author"],
-// };
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 3;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "yota",
-    age: 25,
-    hobbies: ["soccer", "cooking"],
-    role: Role.ADMIN,
-};
-if (person.role === Role.ADMIN) {
-    console.log("読み取り専用ユーザー");
+function combine(input1, input2) {
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-// person.role.push("admin");
-// person.role[1] = 10;
-// person.role = [0, "admin", "aaa"];
-// let favoriteActivities: string[];
-// favoriteActivities = ["aaa"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map());
-}
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combinedName = combine("Max", "Annna");
+console.log(combinedName);
