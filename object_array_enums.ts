442ce34039671d11a131d1b572c42b435be019ad
yuki1582 <yuki.nakamura1103@gmail.com>
2023-10-17 23:54:19 +0900

    tsc basic

--- object_array_enums.ts
+++ object_array_enums.ts
@@ -9,32 +9,38 @@
 //     hobbies: ["soccer", "cooking"],
 //     role: [2, "author"],
 // };
+
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 3;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
+
+// 複数の定数が必要になったとき
+enum Role {
+  ADMIN, 
+  READ_ONLY, 
+  AUTHOR
+}
+
+const person = {
     name: "yota",
     age: 25,
     hobbies: ["soccer", "cooking"],
     role: Role.ADMIN,
 };
+
 if (person.role === Role.ADMIN) {
-    console.log("読み取り専用ユーザー");
+    console.log("読み取り専用ユーザー" );
 }
 // person.role.push("admin");
 // person.role[1] = 10;
+
 // person.role = [0, "admin", "aaa"];
+
 // let favoriteActivities: string[];
 // favoriteActivities = ["aaa"];
 console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());
 }
